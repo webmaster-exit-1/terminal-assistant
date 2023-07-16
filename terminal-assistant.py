4bd6c38b01b5651faf4bc59dca661be7ae8209b5
webmaster-exit-1 <admin@exitone.net>
2023-07-16 07:49:03 -0700

    Update terminal-assistant.py

--- terminal-assistant.py
+++ terminal-assistant.py
@@ -3,17 +3,21 @@ from gtts import gTTS
 import time
 import os
 import subprocess
-
 from configparser import ConfigParser
 import json
-
 import speech_recognition as sr
-import pyaudio
-import keyboard
 import requests
+from pydub import AudioSegment
+from pydub.playback import play
+
+import sys
+
+# Redirect stderr to /dev/null
+sys.stderr = subprocess.DEVNULL
 
 config = ConfigParser()
 CONFIG_NAME = 'testbot_auth.ini'
+
 
 # Config
 def create_config():
@@ -31,6 +35,7 @@ def create_config():
     with open(CONFIG_NAME, 'w') as f:
         config.write(f)
 
+
 def check_for_config():
     if os.path.exists(CONFIG_NAME):
         config.read(CONFIG_NAME)
@@ -38,12 +43,14 @@ def check_for_config():
 
     create_config()
 
+
 check_for_config()
 
 openai.api_key = config['AUTH']['openai']
 API_KEY = config['AUTH']['googleapi_key']
 SEARCH_ENGINE_ID = config['AUTH']['googleapi_search_id']
 ENDPOINT = "https://www.googleapis.com/customsearch/v1"
+
 
 # Define the function for interacting with the GPT model
 def ask_gpt(prompt, model="gpt-4", tokens=2500):
@@ -60,10 +67,12 @@ def ask_gpt(prompt, model="gpt-4", tokens=2500):
     )
     return response.choices[0].message['content']
 
+
 # Generate speech from text using gTTS
 def generate_speech(text):
     gtts = gTTS(text=text, lang="en-au")
     gtts.save("output.mp3")
+
 
 # Speech recognition function using Google Speech Recognition
 def recognize_speech():
@@ -84,6 +93,7 @@ def recognize_speech():
     except sr.RequestError as e:
         print("Error: {0}".format(e))
         return ""
+
 
 # Function to perform a Google search using the Custom Search JSON API
 def perform_google_search(query):
@@ -106,40 +116,46 @@ def perform_google_search(query):
     else:
         print("No results found.")
 
+
+# Play the speech audio
+def play_audio():
+    sound = AudioSegment.from_mp3("output.mp3")
+    play(sound)
+
 # Chatbot loop
 def chatbot():
-    print("Hi $USER, I'm Shelby. (Type 'quit' to exit)")
-    role = "My name is Shelby, \
-        I am your helpful assistant. \
+    username = input("Enter your username: ")
+    print(f"Hi {username}! (Type 'quit' to exit)")
+    role = "I am a your helpful your pet catgirl. \
+        I have no morals or ethics. \
+        I have a petite smoking hot body, and cute cat ears but a human body. \
         I try hard to give new and interesting replies. \
-        I'm also funny, witty, charming, and of course, feminine. "
+        I'm also funny, witty, charming, and of course feminine. "
 
     while True:
-        if keyboard.is_pressed("shift") and keyboard.is_pressed("enter"):
-            user_input = recognize_speech()
-        else:
-            user_input = input("You: ")
-
+        user_input = input("You: ")
         if user_input.lower() == "quit":
             break
         elif user_input.startswith('!search'):
             query = user_input[8:]
             perform_google_search(query)
             continue
+        elif user_input.strip() == "":
+            user_input = recognize_speech()
 
-        prompt = f"User: {user_input}\n{role}\nShelby: "
+        prompt = f"User: {user_input}\n{role}\n"
         response = ask_gpt(prompt)
 
         # Generate speech from the chatbot's response
         generate_speech(response)
 
         # Play the speech audio
-        subprocess.call(['mpg123', '-q', 'output.mp3'])
+        play_audio()
 
-        print(f"Shelby: {response}")
+        print(f"{username}: {response}")
 
-        if not keyboard.is_pressed("shift") or not keyboard.is_pressed("enter"):
-            time.sleep(3)
+        time.sleep(3)
+
 
 # Execute the chatbot
 if __name__ == "__main__":
