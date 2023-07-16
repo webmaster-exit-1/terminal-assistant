a3ba7ac3b3483502167c1ddd6c2ac9b277af9039
webmaster-exit-1 <anarchynjw@gmail.com>
2023-07-16 15:19:12 -0700

    commit

--- terminal-assistant.py
+++ terminal-assistant.py
@@ -3,16 +3,20 @@ from gtts import gTTS
 import time
 import os
 import subprocess
-
 from configparser import ConfigParser
-import json
-
 import speech_recognition as sr
-import pyaudio
-import keyboard
+import requests
+from pydub import AudioSegment
+from pydub.playback import play
+
+import sys
+
+# Redirect stderr to /dev/null
+sys.stderr = subprocess.DEVNULL
 
 config = ConfigParser()
 CONFIG_NAME = 'testbot_auth.ini'
+
 
 # Config
 def create_config():
@@ -30,12 +34,14 @@ def create_config():
     with open(CONFIG_NAME, 'w') as f:
         config.write(f)
 
+
 def check_for_config():
     if os.path.exists(CONFIG_NAME):
         config.read(CONFIG_NAME)
         return
 
     create_config()
+
 
 check_for_config()
 
@@ -44,12 +50,13 @@ API_KEY = config['AUTH']['googleapi_key']
 SEARCH_ENGINE_ID = config['AUTH']['googleapi_search_id']
 ENDPOINT = "https://www.googleapis.com/customsearch/v1"
 
+
 # Define the function for interacting with the GPT model
 def ask_gpt(prompt, model="gpt-4", tokens=2500):
     response = openai.ChatCompletion.create(
         model=model,
         messages=[
-            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "system", "content": "I am your helpful assistant"},
             {"role": "user", "content": prompt}
         ],
         max_tokens=tokens,
@@ -58,6 +65,7 @@ def ask_gpt(prompt, model="gpt-4", tokens=2500):
         temperature=0.7,
     )
     return response.choices[0].message['content']
+
 
 # Generate speech from text using gTTS
 def generate_speech(text):
@@ -107,40 +115,44 @@ def perform_google_search(query):
     else:
         print("No results found.")
 
+
+# Play the speech audio
+def play_audio():
+    sound = AudioSegment.from_mp3("output.mp3")
+    play(sound)
+
+
 # Chatbot loop
 def chatbot():
-    print("Hi $USER, I'm Shelby. (Type 'quit' to exit)")
-    role = "My name is Shelby, \
-        I am your helpful assistant. \
+    username = input("Enter your username: ")
+    print(f"Hi {username}! (Type 'quit' to exit)")
+    role = "I am a your helpful assistant. \
         I try hard to give new and interesting replies. \
-        I'm also funny, witty, charming, and of course, feminine. "
+        I'm also funny, witty, charming, and a great programmer. "
 
     while True:
-        if keyboard.is_pressed("shift+enter"):
-            user_input = recognize_speech()
-        else:
-            user_input = input("You: ")
-
+        user_input = input("You: ")
         if user_input.lower() == "quit":
             break
         elif user_input.startswith('!search'):
             query = user_input[8:]
             perform_google_search(query)
             continue
+        elif user_input.strip() == "":
+            user_input = recognize_speech()
 
-        prompt = f"User: {user_input}\n{role}\nShelby: "
+        prompt = f"User: {user_input}\n{role}\n"
         response = ask_gpt(prompt)
 
         # Generate speech from the chatbot's response
         generate_speech(response)
 
         # Play the speech audio
-        os.system("mpg123 -q output.mp3")
+        play_audio()
 
-        print(f"Shelby: {response}")
+        print(f"{username}: {response}")
 
-        if not keyboard.is_pressed("shift+enter"):
-            time.sleep(3)
+        time.sleep(3)
 
 
 # Execute the chatbot
