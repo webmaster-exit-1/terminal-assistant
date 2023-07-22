10e8dd051205840d2a4c1e0322a87a6a21215272
webmaster-exit-1 <admin@exitone.net>
2023-07-21 21:34:32 -0700

    Create test_terminal_assistant.py

---
+++ test/test_terminal_assistant.py
@@ -0,0 +1,45 @@
+import unittest
+from unittest.mock import patch
+from your_module_name import (ask_gpt, recognize_speech,
+                              perform_google_search, generate_speech, chatbot)
+
+
+class TestTerminalAssistant(unittest.TestCase):
+
+    def test_gpt_interaction(self):
+        # Test GPT interaction with a sample prompt
+        prompt = "Hello, how are you?"
+        response = ask_gpt(prompt)
+        self.assertIsInstance(response, str)
+        self.assertGreater(len(response), 0)
+
+    def test_speech_recognition(self):
+        # Test speech recognition functionality
+        with patch("builtins.input", side_effect=["Hello"]):
+            recognized_text = recognize_speech()
+        self.assertIsInstance(recognized_text, str)
+        self.assertEqual(recognized_text.lower(), "hello")
+
+    def test_google_search(self):
+        # Test the Google search function
+        query = "Chatbots and AI"
+        perform_google_search(query)
+        # Assert that no errors occur, no specific assertions for the output since it varies.
+
+    def test_speech_generation(self):
+        # Test speech generation functionality
+        response = "Sure, I can generate speech!"
+        generate_speech(response)
+        # Assert that speech is generated without errors, no specific assertions for the output.
+
+    def test_chatbot_main_loop(self):
+        # Test the main chatbot loop with mock inputs and responses
+        with patch("builtins.input", side_effect=["John", "!search Python", "", "quit"]):
+            with patch("builtins.print") as mocked_print:
+                chatbot()
+                # Assert that print is called with the expected output for each interaction.
+                mocked_print.assert_called_with("John: ...")
+
+
+if __name__ == "__main__":
+    unittest.main()
